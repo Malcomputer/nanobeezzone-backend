b228548c010dc35a2a4eaa0fa0aa5754f4117f03
Malcomputer <malcolmmkubwa@gmail.com>
2021-04-07 04:33:07 -0500

    crypto, jwt and mongodb package install

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
+const MongoClient = require('mongodb').MongoClient;
+const jwt = require('jsonwebtoken');
 const express = require('express');
+const crypto = require('crypto');
 const app = express();
 const port = process.env.PORT || 3001;
+app.use(express.json());
+
+const getHashedPassword = password => {
+	const sha256 = crypto.createHash('sha256');
+	return sha256.update(password).digest('base64');
+}
+
+const client = new MongoClient(process.env.DB, {useNewUrlParser: true, useUnifiedTopology: true});
 
 app.get('/', (req, res) => {
 	res.send('Server Running');
