f7da6b3d424c6bd09bb368f958232abc4e08d564
Malcomputer <malcolmmkubwa@gmail.com>
2021-04-07 22:30:54 -0500

    fix cors error

--- index.js
+++ index.js
@@ -8,6 +8,19 @@ require('dotenv').config();
 
 app.use(express.json());
 
+app.use(function (req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept"
+	);
+	res.header(
+		"Access-Control-Allow-Methods",
+		"GET, POST, OPTIONS, PUT, PATCH, DELETE"
+	);
+	next();
+});
+
 const getHashedPassword = password => {
 	const sha256 = crypto.createHash('sha256');
 	return sha256.update(password).digest('base64');
